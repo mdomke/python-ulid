add528ae2988f48caab0f6b86101de97af42dc1e
avihais12344 <avihais12344@gmail.com>
2026-03-05 16:50:45 +0200

    Added the same timestamp tests for many value providers.

--- tests/value_provider/test_timestamp_value_base_provider.py
+++ tests/value_provider/test_timestamp_value_base_provider.py
@@ -10,6 +10,8 @@ from freezegun import freeze_time
 from tests.conftest import utcnow
 from ulid import constants
 from ulid.value_provider.abstract_value_provider import AbstractValueProvider
+from ulid.value_provider.monotonic_value_provider import MonotonicValueProvider
+from ulid.value_provider.non_monotonic_value_provider import NonMonotonicValueProvider
 
 
 class TestValueProvider(AbstractValueProvider):
@@ -25,12 +27,19 @@ class TestValueProvider(AbstractValueProvider):
             datetime(2025, 12, 31, 9, 6, 3, tzinfo=timezone.utc),
         ],
 )
-def test_timestamp(datetime_timestamp: datetime) -> None:
-    provider = TestValueProvider()
+@pytest.mark.parametrize(
+    "value_provider",
+    [
+        pytest.param(TestValueProvider(), id="CustomedValueProvider"),
+        pytest.param(MonotonicValueProvider(), id="MonotonicValueProvider"),
+        pytest.param(NonMonotonicValueProvider(), id="NonMonotonicValueProvider")
+    ]
+)
+def test_timestamp(datetime_timestamp: datetime, value_provider: AbstractValueProvider,) -> None:
     expected_timestamp = int(datetime_timestamp.timestamp() * constants.MILLISECS_IN_SECS)
 
-    first_timestamp = provider.timestamp(datetime_timestamp.timestamp())
-    second_timestamp = provider.timestamp(datetime_timestamp.timestamp())
+    first_timestamp = value_provider.timestamp(datetime_timestamp.timestamp())
+    second_timestamp = value_provider.timestamp(datetime_timestamp.timestamp())
 
     assert isinstance(first_timestamp, int)
     assert first_timestamp == expected_timestamp
@@ -40,15 +49,21 @@ def test_timestamp(datetime_timestamp: datetime) -> None:
 
 
 @freeze_time()
-def test_timestamp_now() -> None:
-    provider = TestValueProvider()
-
+@pytest.mark.parametrize(
+    "value_provider",
+    [
+        pytest.param(TestValueProvider(), id="CustomedValueProvider"),
+        pytest.param(MonotonicValueProvider(), id="MonotonicValueProvider"),
+        pytest.param(NonMonotonicValueProvider(), id="NonMonotonicValueProvider")
+    ]
+)
+def test_timestamp_now(value_provider: AbstractValueProvider,) -> None:
     with freeze_time() as frozen:
         expected_first_timestamp = int(utcnow().timestamp() * constants.MILLISECS_IN_SECS)
-        first_timestamp = provider.timestamp()
+        first_timestamp = value_provider.timestamp()
         frozen.tick()
         expected_second_timestamp = int(utcnow().timestamp() * constants.MILLISECS_IN_SECS)
-        second_timestamp = provider.timestamp()
+        second_timestamp = value_provider.timestamp()
 
     assert isinstance(first_timestamp, int)
     assert first_timestamp == expected_first_timestamp
@@ -57,8 +72,14 @@ def test_timestamp_now() -> None:
     assert second_timestamp > first_timestamp
 
 
-def test_max_timestamp() -> None:
-    provider = TestValueProvider()
-
+@pytest.mark.parametrize(
+    "value_provider",
+    [
+        pytest.param(TestValueProvider(), id="CustomedValueProvider"),
+        pytest.param(MonotonicValueProvider(), id="MonotonicValueProvider"),
+        pytest.param(NonMonotonicValueProvider(), id="NonMonotonicValueProvider")
+    ]
+)
+def test_max_timestamp(value_provider: AbstractValueProvider,) -> None:
     with pytest.raises(ValueError, match="Value exceeds maximum possible timestamp"):
-        provider.timestamp(constants.MAX_TIMESTAMP + 1)
+        value_provider.timestamp(constants.MAX_TIMESTAMP + 1)
